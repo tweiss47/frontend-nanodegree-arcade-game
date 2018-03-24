ae8b5cd633eb8f844bcd30f5c7f605a3a4f8aec8
Taylor Weiss <tweiss@microsoft.com>
2018-03-23 17:03:44 -0700

    Stub out enough so that the world will display

--- js/app.js
+++ js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x = 0;
+    this.y = 0;
 };
 
 // Update the enemy's position, required method for game
@@ -21,15 +23,29 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Our player
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png'
+    this.x = 0;
+    this.y = 0;
+};
+
+Player.prototype.update = function(dt) {
+};
+
+Player.prototype.render = function() {
+};
+
+Player.prototype.handleInput = function() {
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+var allEnemies = [];
 
+// Place the player object in a variable called player
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
