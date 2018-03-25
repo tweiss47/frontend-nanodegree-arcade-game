5ec0aa61718f1cdd8e2b1ae275e7bf7ba4ae7aea
Taylor Weiss <tweiss@microsoft.com>
2018-03-25 09:16:18 -0700

    Handle collisions

--- js/app.js
+++ js/app.js
@@ -1,5 +1,3 @@
-// Contants and utilities
-
 // Return a random integer in the range [min .. max)
 var randomInteger = function(min, max) {
     min = Math.ceil(min);
@@ -11,10 +9,12 @@ var randomInteger = function(min, max) {
 // Enemies and Players
 var Actor = function(sprite) {
     this.sprite = Resources.get(sprite);
+    this.yOffset = 0;
     this.x = 0;
     this.y = 0;
 };
 
+// Common render code for all actors
 Actor.prototype.render = function() {
     ctx.drawImage(this.sprite, this.x, this.y);
 }
@@ -23,22 +23,29 @@ Actor.prototype.update = function(dt) {
     // Do nothing by default
 };
 
+// What row is the Actor in 0 is the top of the road
+Actor.prototype.getRow = function() {
+    return Math.floor((this.y + this.yOffset) / 83);
+}
+
 // Enemies our player must avoid
 var Enemy = function() {
     Actor.call(this, 'images/enemy-bug.png');
     this.speed = 0;
+    this.yOffset = -20;
 
     // Set the position and speed of the Enemy for the start of a run
     // Each enemy starts off the screen some random amount so that
     // the all the enemies appear on the screen at different times
     this.reset = function() {
         this.x = randomInteger(-1600, -100);
-        this.y = 83 * randomInteger(1, 4) - 20;
+        this.y = 83 * randomInteger(1, 4) + this.yOffset;
         this.speed = randomInteger(150, 300);
     };
     this.reset();
 };
 
+// Inherit from Actor
 Enemy.prototype = Object.create( Actor.prototype, {
         constructor: {
             configurable: true, enumerable: true, value: Enemy, writeable: true
@@ -52,23 +59,33 @@ Enemy.prototype.update = function(dt) {
     // Move
     this.x += dt * this.speed;
 
+    // Are we contacting a player
+    if (this.getRow() === player.getRow()) {
+        if (this.x + 60 > player.x && // front of enemy overlaps the left
+            this.x < player.x + 80 // back of enemy overlaps the right
+        ) {
+            player.reset();
+        }
+    }
+
     // Once we've moved off the screen some we reset
     if (this.x > 500) {
         this.reset();
     }
 };
 
-// Our player
+// Object type for our player
 var Player = function() {
     Actor.call(this, 'images/char-boy.png');
+    this.yOffset = -10;
     this.reset = function() {
         this.x = 200;
-        this.y = 5 * 83 - 10;
+        this.y = 5 * 83 + this.yOffset;
     }
     this.reset();
-    console.log(this.x, this.y);
 };
 
+// Inherit from Actor
 Player.prototype = Object.create( Actor.prototype, {
         constructor: {
             configurable: true, enumerable: true, value: Player, writeable: true
@@ -76,7 +93,9 @@ Player.prototype = Object.create( Actor.prototype, {
     }
 )
 
+// Move the player in response to key presses
 Player.prototype.handleInput = function(key) {
+    // For each direction test to see if the we are already on the border
     switch(key) {
         case 'left':
             if (this.x > 0) {
@@ -100,7 +119,6 @@ Player.prototype.handleInput = function(key) {
             break;
     }
 
-    console.log("Player (" + this.x + ", " + this.y + ")")
     // Reached the water
     if (this.y < 0) {
         this.reset();
